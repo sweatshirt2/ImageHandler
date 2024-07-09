80b51290889bd20034c88454ef46aaa1d1790dea
sweatshirtblue2 <sweatshirtblue2@gmail.com>
2024-07-09 09:02:20 +0000

    done with the image retrieval

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
-const fs = require('fs');
-const path = require('path');
-const multer = require('multer');
-const express = require('express');
+import multer from 'multer';
+import express from 'express';
+import { access, constants } from 'fs';
+import path, { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 const app = express();
 const PORT = parseInt(process.env.PORT) || 3000;
@@ -12,9 +16,10 @@ const idb = multer.diskStorage({
         cbf(null, path.join(__dirname, 'db', 'images'));
     },
     filename: (rq, file, cbf) => {
-        cbf(null, Date.now() + path.extname(file.originalname));
+      cbf(null, rq.body.image_id + path.extname(file.originalname));
     },
 });
+
 
 const upload = multer({ storage: idb });
 
@@ -28,16 +33,41 @@ app.post('/upload', upload.single('image'), (rq, rs) => {
     rs.send('Image uploaded successfully!');
 });
 
-// app.get('/download/:filename', (rq, rs) => {
-//     const fname = rq.params.filename;
-//     const fpath = path.join(__dirname, 'db/images', fname);
-  
-//     if (fs.existsSync(fpath)) {
-//       rs.download(fpath);
+// app.get('/image', (rq, rs) => {
+//   const { image_id } = rq.query;
+
+//   if (!image_id) {
+//     return rs.status(400).json({ error: 'Image id is required' });
+//   }
+//   const file_path = path.join(__dirname, 'db', 'images', image_id + '.png');
+//   console.log(file_path);
+//   stat(file_path, (stats) => {
+//     if (stats) {
+//       rs.sendFile(file_path);
 //     } else {
-//       rs.status(404).send('File not found');
+//       rs.status(404).json({ error: 'File not found' });
 //     }
 //   });
+// });
+
+app.get('/image', (rq, rs) => {
+  const { image_id } = rq.query;
+
+  if (!image_id) {
+    return rs.status(400).json({ error: 'Image id is required' });
+  }
+
+  const file_path = path.join(__dirname, 'db', 'images', image_id + '.png'); 
+
+  access(file_path, constants.F_OK, (error) => {
+    if (error) {
+      rs.status(404).json({ error: 'File not found' });
+    } else {
+      rs.sendFile(file_path);
+    }
+  });
+});
+
 
 app.listen(PORT, () => {
     console.log(`Server running at http://localhost:${PORT}`);
